a07d53ea8e3c3f64f2ff8fb0d148bab2226f6667
Felippe <felippe.moraes@zoho.com>
2020-03-23 01:04:00 -0300

    sync

---
+++ dist/precache-manifest.9ca40f16a87a8822eacb11d67c13abde.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "606a976bea3490c28930",
+    "url": "/memory-lights/css/app.914e0517.css"
+  },
+  {
+    "revision": "c21d2ecdf0b425975d0b",
+    "url": "/memory-lights/css/chunk-vendors.391ed6f9.css"
+  },
+  {
+    "revision": "3889ece508241673cc04198f401a71ff",
+    "url": "/memory-lights/img/memory-lights.png"
+  },
+  {
+    "revision": "bdfe4c803f89052ba57da3ec69e0059b",
+    "url": "/memory-lights/index.html"
+  },
+  {
+    "revision": "606a976bea3490c28930",
+    "url": "/memory-lights/js/app.d546e853.js"
+  },
+  {
+    "revision": "c21d2ecdf0b425975d0b",
+    "url": "/memory-lights/js/chunk-vendors.3f8bd540.js"
+  },
+  {
+    "revision": "3089e546e1c2e28738cb8ead3a2dd1c0",
+    "url": "/memory-lights/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/memory-lights/robots.txt"
+  }
+]);
